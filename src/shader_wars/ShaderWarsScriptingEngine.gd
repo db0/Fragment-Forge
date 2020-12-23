2a8642ee633227ff5fc8e30bdcf5a8fb15f44ff5
Divided by Zer0 <mail@dbzer0.com>
2020-12-24 00:44:42 +0100

    tweaks for 1.3

--- src/shader_wars/ShaderWarsScriptingEngine.gd
+++ src/shader_wars/ShaderWarsScriptingEngine.gd
@@ -13,11 +13,11 @@ func _init(card_owner: Card,
 func modify_counter(script: ScriptTask) -> int:
 	var counter_name: String = script.get_property(SWSP.KEY_COUNTER)
 	var modification: int
-	if str(script.get_property(SP.KEY_TOKEN_MODIFICATION)) == SP.VALUE_RETRIEVE_INTEGER:
+	if str(script.get_property(SP.KEY_MODIFICATION)) == SP.VALUE_RETRIEVE_INTEGER:
 		modification = stored_integer
 	else:
-		modification = script.get_property(SP.KEY_TOKEN_MODIFICATION)
-	var set_to_mod: bool = script.get_property(SP.KEY_TOKEN_SET_TO_MOD)
+		modification = script.get_property(SP.KEY_MODIFICATION)
+	var set_to_mod: bool = script.get_property(SP.KEY_SET_TO_MOD)
 	var retcode: int = cfc.NMAP.board.counters.mod_counter(
 			counter_name,
 			modification,
